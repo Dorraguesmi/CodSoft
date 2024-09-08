c2a4ab45fb63088aab56c84d7a17c00151257cc5
dorra <dorragasmi1@gmail.com>
2024-09-08 13:14:41 +0100

    update

--- ProjectManagementTool/frontend/src/App.js
+++ ProjectManagementTool/frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
-import './App.css';
+import "./App.css";
 
 const App = () => {
   const [projects, setProjects] = useState([]);
@@ -39,11 +40,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>Project Management Tool</h1>
+      {/* <h1>Project Management Tool</h1>
       <ProjectForm />
       <ProjectList projects={projects} />
       <TaskForm projects={projects} addTask={addTask} />
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} /> */}
     </div>
   );
 };
